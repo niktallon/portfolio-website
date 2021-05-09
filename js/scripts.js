7bf0b38a54ce22db731e0061b9249fda37906f55
niktallon <nicole@nicoletallon.com>
2021-05-09 13:47:22 -0700

    One page website and nav/footer updates
    -Resume now opens in new tab
    -"Work" tab added to nav
    -"About" content merged into homepage
    -Project nav updated
    -Behance and Dribble added to footer - Twitter removed
    -Replaced logo in footer with my avatar
    -Centered resume button
    -Contact button replaced with copy function

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,25 @@
-
 // hamburger menu
-function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
+function menuToggle () {
+  var x = document.getElementById('myNavtoggle')
   if (x.className === 'navtoggle') {
-    x.className += ' responsive';
+    x.className += ' responsive'
   } else {
-    x.className = 'navtoggle';
+    x.className = 'navtoggle'
   }
 }
 
+// copypaste function
+var clipboard = new Clipboard('.btn');
+
+clipboard.on('success', function(e) {
+	console.log(e);
+});
+
+clipboard.on('error', function(e) {
+    console.log(e);
+});
+
+/* hide smooth scrolling
 // Smooth scrolling
 
 // Select all links with hashes
@@ -16,34 +27,35 @@ $('a[href*="#"]')
   // Remove links that don't actually link to anything
   .not('[href="#"]')
   .not('[href="#0"]')
-  .click(function(event) {
+  .click(function (event) {
     // On-page links
     if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-      &&
-      location.hostname == this.hostname
+      location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') &&
+      location.hostname === this.hostname
     ) {
       // Figure out element to scroll to
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      var target = $(this.hash)
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']')
       // Does a scroll target exist?
       if (target.length) {
         // Only prevent default if animation is actually gonna happen
-        event.preventDefault();
+        event.preventDefault()
         $('html, body').animate({
           scrollTop: target.offset().top
-        }, 1000, function() {
+        }, 1000, function () {
           // Callback after animation
           // Must change focus!
-          var $target = $(target);
-          $target.focus();
+          var $target = $(target)
+          $target.focus()
           if ($target.is(":focus")) { // Checking if the target was focused
             return false;
           } else {
-            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.attr('tabindex' ,'-1'); // Adding tabindex for elements not focusable
             $target.focus(); // Set focus again
           };
         });
       }
     }
-  });
+  })
+
+  */
